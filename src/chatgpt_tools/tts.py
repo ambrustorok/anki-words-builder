890eb31f8981d930a3c12d777c115fea21bc7c0e
Ambrus Török <ambrustorok98@gmail.com>
2024-10-29 21:28:21 +0100

    bugfix: special characters on input no longer cause file error

--- src/chatgpt_tools/tts.py
+++ src/chatgpt_tools/tts.py
@@ -3,7 +3,15 @@ import tempfile
 import numpy as np
 import soundfile as sf
 import random
+import re
+import uuid
 
+def sanitize_filename(text):
+    # Replace special characters with underscores
+    sanitized_text = re.sub(r'[^\w\s-]', '_', text)
+    # Replace spaces with underscores
+    sanitized_text = sanitized_text.replace(' ', '_')
+    return sanitized_text
 
 def add_silence(input_file, output_file, silence_duration_sec=0.5):
     data, samplerate = sf.read(input_file)
@@ -16,20 +24,15 @@ def add_silence(input_file, output_file, silence_duration_sec=0.5):
     new_data = np.concatenate((silence, data), axis=0)
     sf.write(output_file, new_data, samplerate)
 
-
 def remove_audio_start(input_file, output_file, duration_sec):
     # Read the audio file
     data, samplerate = sf.read(input_file)
-
     # Calculate the number of samples to remove
     samples_to_remove = int(duration_sec * samplerate)
-
     # Remove the first x seconds
     new_data = data[samples_to_remove:]
-
     # Write the modified audio to the output file
     sf.write(output_file, new_data, samplerate)
-
 
 def get_audio(
     client, text, lang="da", output_dir="/mnt/d/OneDrive/Projects/data/anki_audio"
@@ -48,9 +51,13 @@ def get_audio(
             response.stream_to_file(temp_audio_path)
 
     os.makedirs(output_dir, exist_ok=True)
-    output_file_path = os.path.join(output_dir, f"audio_{text}.mp3")
-    # TODO: make these configurable
-    add_silence(temp_audio_path, output_file_path, silence_duration_sec=0)
-    # remove_audio_start(temp_audio_path, output_file_path, duration_sec=1.07)
+    sanitized_text = sanitize_filename(text)
+    unique_id = uuid.uuid4().hex
+    output_file_path = os.path.join(output_dir, f"audio_{sanitized_text}_{unique_id}.mp3")
+
+    # Read the audio file
+    data, samplerate = sf.read(temp_audio_path)
+    # Write the audio to the output file without adding silence
+    sf.write(output_file_path, data, samplerate)
     os.remove(temp_audio_path)
     return output_file_path
