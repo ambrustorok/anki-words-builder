4aeb5c31a0ba65328262499a1ca2e2acee45821a
Ambrus Török <ambrustorok98@gmail.com>
2025-02-13 19:00:26 +0100

    fix: audio not working on iOS

---
+++ src/utils/__init__.py
@@ -0,0 +1,1 @@
+from .utils import convert_audio_to_numpy, get_read_aloud_text, string_to_html_div, convert_string_to_html
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,3 +1,8 @@
+import numpy as np
+import pydub
+import tempfile
+
+
 def string_to_html_div(input_string):
     if not isinstance(input_string, str):
         raise ValueError("Input must be a string")
@@ -35,3 +40,20 @@ def get_read_aloud_text(lang):
         "danish": "Læs dette på dansk",
     }
     return texts[lang.lower()]
+
+def convert_audio_to_numpy(audio_bytes):
+    if audio_bytes is None:
+        return None
+        
+    with tempfile.NamedTemporaryFile(suffix=".mp3", delete=True) as temp_file:
+        temp_file.write(audio_bytes)
+        temp_file.flush()
+        
+        # Convert MP3 file to NumPy array, ensuring correct sample rate and mono
+        audio = pydub.AudioSegment.from_file(temp_file.name, format="mp3").set_frame_rate(44100).set_channels(1)
+        samples = np.array(audio.get_array_of_samples(), dtype=np.float32)
+        if audio.channels > 1:
+            samples = samples.reshape((-1, audio.channels)).mean(axis=1)  # Convert stereo to mono
+        samples /= np.iinfo(np.int16).max  # Normalize to range [-1, 1]
+    
+    return samples  # Return just the samples, as rate is fixed at 44100
