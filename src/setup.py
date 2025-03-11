0d8a11bbb3875b22ad9a804e53f380df66fae2f2
Ambrus Török <ambrustorok98@gmail.com>
2025-03-11 20:56:06 +0100

    refactor: revert to smaller models

--- src/setup.py
+++ src/setup.py
@@ -1,15 +1,15 @@
-
 import os
 from dotenv import load_dotenv
 
 from openai import OpenAI
+
 load_dotenv()
 
 openai_client = OpenAI()
-openai_model="gpt-4o"
+openai_model = "gpt-4o-mini"
 
-host=os.getenv('POSTGRES_HOST', 'localhost')
-database=os.getenv('POSTGRES_DB', "postgres")
-user=os.getenv('POSTGRES_USER')
-password=os.getenv('POSTGRES_PASSWORD')
-port=os.getenv('POSTGRES_PORT', '5432')
+host = os.getenv("POSTGRES_HOST", "localhost")
+database = os.getenv("POSTGRES_DB", "postgres")
+user = os.getenv("POSTGRES_USER")
+password = os.getenv("POSTGRES_PASSWORD")
+port = os.getenv("POSTGRES_PORT", "5432")
